bbfcb9bf423f853f7b30b9c3da8f2a39d3bd9483
Maulana Bagus Sudrajat <maulana712bagus@gmail.com>
2026-02-28 10:32:46 +0700

    Fix build and add secret migrate route

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,15 @@ use App\Http\Controllers\Auth\LoginController;
 use Illuminate\Support\Facades\Route;
 
 // AUTH
+Route::get('/rahasia-migrate-sekarang', function () {
+    try {
+        \Illuminate\Support\Facades\Artisan::call('migrate:fresh --seed --force');
+        return "Berhasil Mas! Database sudah terisi semua tabel & data awal. Silakan balik ke Beranda.";
+    } catch (\Exception $e) {
+        return "Error: " . $e->getMessage();
+    }
+});
+
 Route::get('/login', [LoginController::class , 'showLoginForm'])->name('login');
 Route::post('/login', [LoginController::class , 'login']);
 Route::post('/logout', [LoginController::class , 'logout'])->name('logout');
